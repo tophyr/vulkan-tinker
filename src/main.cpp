81a700e66db29d2c1eefe99b36df9f75c88bf6e2
Chris Sarbora <tophyr@gmail.com>
2025-07-19 17:07:54 -0500

    create vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include "glfw.hpp"
+#include "vulkan.hpp"
+
+constexpr char const* kName{"Vulkan Tinker"};
 
 int main() {
   glfw::GlobalState glfwState;
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
   {
-    glfw::Window window{1920, 1080, "Vulkan Tinker"};
+    glfw::Window window{1920, 1080, kName};
+    vk::Instance instance{kName};
 
     while (!glfwWindowShouldClose(window)) {
       glfwPollEvents();
