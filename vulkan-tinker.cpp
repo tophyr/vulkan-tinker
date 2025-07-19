86a5d723d2d0d6745f2bf3d8fc81da17e9d4f542
Chris Sarbora <tophyr@gmail.com>
2025-07-19 01:51:56 -0500

    initial commit

---
+++ vulkan-tinker.cpp
@@ -0,0 +1,12 @@
+﻿// vulkan-tinker.cpp : Defines the entry point for the application.
+//
+
+#include "vulkan-tinker.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
